db5d4e49ba6e3076beec112f4d3e415bbb1aa84c
Rauno Tali <rauno@pats.ee>
2025-05-02 01:33:27 +0300

    finish restyling page to match new company cvi

--- index.php
+++ index.php
@@ -8,72 +8,69 @@ $request = new Request($_REQUEST);
 $uri = $_SERVER['REQUEST_URI'];
 
 $cmd = $request->param('cmd')
-    ? $request->param('cmd')
-    : '';
+  ? $request->param('cmd')
+  : '';
 
 $lng = $request->param('lng')
-    ? $request->param('lng')
-    : '';
+  ? $request->param('lng')
+  : '';
 
 // Comment out when going live
-//$pattern = '/(\w+)\/(\D+)$/';
-//preg_match($pattern, $uri, $matches);
-//if ($matches) {
+// $pattern = '/(\w+)\/(\D+)$/';
+// preg_match($pattern, $uri, $matches);
+// if ($matches) {
 //    $lng = $matches[1];
 //    $cmd = $matches[2];
-//} else {
+// } else {
 //    preg_match('/(\w+)/', $uri, $lngMatch);
 //    if ($lngMatch) {
 //        $lng = $lngMatch[1];
 //    }
-//}
+// }
 
 $data = array(
-    'lng' => $lng,
-    'languages' => getLanguages(),
-    'menuCategories' => getMenuCategories($lng),
-    'navigation' => "navigation.html"
+  'lng' => $lng,
+  'languages' => getLanguages(),
+  'menuCategories' => getMenuCategories($lng),
+  'navigation' => "navigation.html"
 );
 
 $languageShortnames = array_map(function ($l) {
-    return $l['short'];
+  return $l['short'];
 }, $data['languages']);
 
 if (!$data['menuCategories']) {
-    $lng = 'et';
-    $data['lng'] = $lng;
-    $data['menuCategories'] = getMenuCategories($lng);
+  $lng = 'et';
+  $data['lng'] = $lng;
+  $data['menuCategories'] = getMenuCategories($lng);
 }
 
 $menuShortnames = array_map(function ($m) {
-    return $m['uri'];
+  return $m['uri'];
 }, $data['menuCategories']);
 
 if (in_array($lng, $languageShortnames) && in_array($cmd, $menuShortnames)) {
 
-    $menuItems = getMenu($lng, $cmd);
-    $headline = $menuItems['heading'];
-    $data['template'] = "menu.html";
-    $data['menu'] = $menuItems['items'];
-    $data['headline'] = $headline;
-    $data['headingForTitle'] = "$headline | ";
-
+  $menuItems = getMenu($lng, $cmd);
+  $headline = $menuItems['heading'];
+  $data['template'] = "menu.html";
+  $data['menu'] = $menuItems['items'];
+  $data['headline'] = $headline;
+  $data['headingForTitle'] = "$headline | ";
 } else if (in_array($lng, $languageShortnames)) {
 
-    $data['template'] = "{$lng}/ordering.html";
-    $data['headingForTitle'] = match ($lng) {
-        'et' => 'Tellimine | ',
-        'en' => 'Ordering | ',
-        'ru' => 'Заказ | ',
-        'fr' => 'Commande | ',
-        default => '',
-    };
-
+  $data['template'] = "{$lng}/ordering.html";
+  $data['headingForTitle'] = match ($lng) {
+    'et' => 'Tellimine | ',
+    'en' => 'Ordering | ',
+    'ru' => 'Заказ | ',
+    'fr' => 'Commande | ',
+    default => '',
+  };
 } else {
 
-    $data['template'] = "et/ordering.html";
-    $data['headingForTitle'] = 'Tellimine | ';
-
+  $data['template'] = "et/ordering.html";
+  $data['headingForTitle'] = 'Tellimine | ';
 }
 
 print renderTemplate('templates/main.html', $data);
